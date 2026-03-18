dca7d53ad48d58e0709e9657de9b5b1c7d604808
BengoPop <retr0astr4@gmail.com>
2026-03-18 01:23:15 -0400

    refactored code, added proper resetting display as well as chaining calculations but only for addition

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const display = document.querySelector(".display");
-
 let numbersContainer = document.querySelector(".numberButtons")
-
-
 function createNumberedButton(number) {
   let numberButton = document.createElement('button');
   numberButton.innerText = number;
   numberButton.addEventListener("click", (e) => {
-    display.textContent += numberButton.textContent
-  });
+    if (display.textContent === "0" || shouldResetDisplay) {
+      display.textContent = numberButton.textContent
+      shouldResetDisplay = false;
+    } else {
 
+      display.textContent += numberButton.textContent
+    }
+  });
   numberButton.type = "button";
   numbersContainer.append(numberButton);
 }
@@ -17,32 +19,50 @@ for (let i = 0; i < 10; i++) {
   createNumberedButton(i);
 }
 
-const multiplyButton = document.querySelector("#multiply")
-const divideButton = document.querySelector("#divide")
-const subtractButton = document.querySelector("#subtract")
+
+let num1 = 0;
+let num2 = 0;
+let operator;
+let shouldResetDisplay = false;
+
 
 const addButton = document.querySelector("#add")
+
 addButton.addEventListener("click", (e) => {
-  num1 = parseFloat(display.textContent)
-  display.textContent = 0;
-  operator = addButton.textContent;
+  if (!operator) {
+    num1 = parseFloat(display.textContent);
+    operator = addButton.textContent;
+  } else {
+    num2 = parseFloat(display.textContent);
+    operate(operator, num1, num2);
+    operator = addButton.textContent;
+    num1 = parseFloat(display.textContent);
+  }
+  shouldResetDisplay = true;
+
 });
 
 function add(num1, num2) {
-  let total = num1 + num2;
-  display.textContent = total;
+  return num1 + num2;
 }
+
+const clearButton = document.querySelector("#clear")
+clearButton.addEventListener("click", (e) => {
+  num1 = 0; num2 = 0; operator = undefined; display.textContent = 0;
+})
+
 const calculateButton = document.querySelector("#calculate")
 calculateButton.addEventListener("click", (e) => {
-  operate(operator, num1, parseFloat(display.textContent))
+  operate(operator, num1, parseFloat(display.textContent));
+  shouldResetDisplay = true
+  operator = undefined;
 })
-let num1, num2, operator;
 
 
 
 function operate(operator, num1, num2) {
   if (operator === "+") {
-    add(num1, num2);
+    result = add(num1, num2);
   } else if (operator === "-") {
     subtract(num1, num2)
   } else if (operator === "*") {
@@ -50,4 +70,5 @@ function operate(operator, num1, num2) {
   } else {
     divide(num1, num2)
   }
+  display.textContent = result;
 }
